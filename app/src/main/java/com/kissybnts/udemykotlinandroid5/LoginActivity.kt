bfa0dfee0b38ff4bdd4c4f15ae4f4712eb4a360a
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2018-03-01 17:28:44 +0900

    Lecture 71, LoginActivity UI

---
+++ app/src/main/java/com/kissybnts/udemykotlinandroid5/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.kissybnts.udemykotlinandroid5
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun onLoginButtonClicked(view: View) {
+
+    }
+
+    fun onCreateUserButtonClicked(view: View) {
+
+    }
+}
