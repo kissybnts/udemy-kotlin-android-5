8c2e8741af7e9d588032ee1a013a199554eb5613
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2018-03-02 19:58:04 +0900

    Lecture 72, CreateUserActivity UI

--- app/src/main/java/com/kissybnts/udemykotlinandroid5/CreateUserActivity.kt
+++ app/src/main/java/com/kissybnts/udemykotlinandroid5/CreateUserActivity.kt
@@ -1,23 +1,25 @@
 package com.kissybnts.udemykotlinandroid5
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class LoginActivity : AppCompatActivity() {
+class CreateUserActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_create_user)
     }
 
-    fun onLoginButtonClicked(view: View) {
+    fun onUserAvatarClicked(view: View) {
+
+    }
+
+    fun onGenerateBackgroundColorButtonClicked(view: View) {
 
     }
 
     fun onCreateUserButtonClicked(view: View) {
-        val createUserIntent = Intent(this, CreateUserActivity::class.java)
-        startActivity(createUserIntent)
+
     }
 }
