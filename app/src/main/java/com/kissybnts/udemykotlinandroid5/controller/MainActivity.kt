94be1ff3c84556b84f6af9b16c66e41e7455eeb5
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2018-03-07 20:37:01 +0600

    Lecture 86, Sockets

--- app/src/main/java/com/kissybnts/udemykotlinandroid5/controller/MainActivity.kt
+++ app/src/main/java/com/kissybnts/udemykotlinandroid5/controller/MainActivity.kt
@@ -12,18 +12,22 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import com.kissybnts.udemykotlinandroid5.R
 import com.kissybnts.udemykotlinandroid5.service.AuthService
 import com.kissybnts.udemykotlinandroid5.service.UserDataService
 import com.kissybnts.udemykotlinandroid5.utils.BROADCAST_USER_DATA_CHANGE
+import com.kissybnts.udemykotlinandroid5.utils.EMIT_ADD_CHANNEL
+import com.kissybnts.udemykotlinandroid5.utils.URL_SOCKET
 import com.kissybnts.udemykotlinandroid5.utils.hideKeyboard
+import io.socket.client.IO
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val socket = IO.socket(URL_SOCKET)
+
     private val userDataChangeReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             if (AuthService.isAuthed) {
@@ -48,9 +52,22 @@ class MainActivity : AppCompatActivity() {
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
-        hideKeyboard()
+    }
 
+    override fun onResume() {
         LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver, IntentFilter(BROADCAST_USER_DATA_CHANGE))
+        socket.connect()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(userDataChangeReceiver)
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        socket.disconnect()
     }
 
 
@@ -80,21 +97,19 @@ class MainActivity : AppCompatActivity() {
         if (AuthService.isAuthed) {
             val builder = AlertDialog.Builder(this)
             val dialogView = layoutInflater.inflate(R.layout.add_channel_dialog, null)
-            builder.setView(dialogView)
-                    .setPositiveButton("Add") { dialogInterface, i ->
-                        // perform some logic to add channel
-                        val channelName = dialogView.findViewById<EditText>(R.id.channelNameTextAddChannel).text.toString()
-                        val channelDescription = dialogView.findViewById<EditText>(R.id.channelDescriptionTextAddChannel).text.toString()
+            builder.setView(dialogView).setPositiveButton("Add") { dialogInterface, i ->
+                // perform some logic to add channel
+                val channelName = dialogView.findViewById<EditText>(R.id.channelNameTextAddChannel).text.toString()
+                val channelDescription = dialogView.findViewById<EditText>(R.id.channelDescriptionTextAddChannel).text.toString()
 
-                        hideKeyboard()
-
-                    }.setNegativeButton("Cancel") { dialogInterface, i ->
-                        hideKeyboard()
-                    }.show()
+                socket.emit(EMIT_ADD_CHANNEL, channelName, channelDescription)
+            }.setNegativeButton("Cancel") { dialogInterface, i ->
+                // TODO do something when adding channel canceled
+            }.show()
         }
     }
 
     fun onSendMessageButtonClicked(view: View) {
-
+        hideKeyboard()
     }
 }
--- app/src/main/java/com/kissybnts/udemykotlinandroid5/utils/Constants.kt
+++ app/src/main/java/com/kissybnts/udemykotlinandroid5/utils/Constants.kt
@@ -6,6 +6,8 @@ const val URL_REGISTER = "$BASE_URL/account/register"
 const val URL_LOGIN = "$BASE_URL/account/login"
 const val URL_CREATE_USER = "$BASE_URL/user/add"
 fun URL_FIND_USER_BY_EMIL(email: String): String =  "$BASE_URL/user/byEmail/$email"
-
+const val URL_SOCKET = "https://udemykotlinandroid.herokuapp.com"
 
 const val BROADCAST_USER_DATA_CHANGE = "broadcast/userDataChange"
+
+const val EMIT_ADD_CHANNEL = "newChannel"
